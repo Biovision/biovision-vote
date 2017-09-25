06b9b07854bbeaf09c8b548a688ff146b9b212d8
Maxim Khan-Magomedov <maxim.km@gmail.com>
2017-09-25 14:18:17 +0300

    Cosmetic changes

--- db/migrate/20170330000001_create_votes.rb
+++ db/migrate/20170330000001_create_votes.rb
@@ -1,13 +1,21 @@
-class CreateVotes < ActiveRecord::Migration[5.0]
-  def change
-    create_table :votes do |t|
-      t.timestamps
-      t.references :user, foreign_key: true, on_update: :cascade, on_delete: :cascade
-      t.references :agent, foreign_key: true, on_update: :cascade, on_delete: :nullify
-      t.inet :ip
-      t.integer :delta, limit: 2, default: 0, null: false
-      t.integer :votable_id, null: false
-      t.string :votable_type, null: false
+class CreateVotes < ActiveRecord::Migration[5.1]
+  def up
+    unless Vote.table_exists?
+      create_table :votes do |t|
+        t.timestamps
+        t.references :user, foreign_key: true, on_update: :cascade, on_delete: :cascade
+        t.references :agent, foreign_key: true, on_update: :cascade, on_delete: :nullify
+        t.inet :ip
+        t.integer :delta, limit: 2, default: 0, null: false
+        t.integer :votable_id, null: false
+        t.string :votable_type, null: false
+      end
+    end
+  end
+
+  def down
+    if Vote.table_exists?
+      drop_table :votes
     end
   end
 end
